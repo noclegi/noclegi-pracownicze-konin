2414bae564bd99978ae33e1eeff5db20df4fbb10
tomaszdurka <tomasz@durka.pl>
2015-03-30 13:20:52 +0200

    Application boostrapped

---
+++ app.js
@@ -0,0 +1,42 @@
+/**
+ * Module dependencies.
+ */
+
+var express  = require('express'),
+    path     = require('path'),
+    config   = require('./config'),
+    routes   = require('./routes');
+
+
+
+var app = express();
+
+
+
+/**
+ * Express configuration.
+ */
+app.set('port', config.server.port);
+app.set('views', path.join(__dirname, 'views'));
+app.set('view engine', 'jade');
+app.use(express.compress());
+app.use(express.favicon());
+app.use(express.logger('dev'));
+app.use(express.json());
+app.use(express.urlencoded());
+app.use(express.methodOverride());
+app.use(app.router);
+app.use(express.static(path.join(__dirname, 'public')));
+app.use(function (req, res) {
+  res.status(404).render('404', {title: 'Not Found :('});
+});
+
+if (app.get('env') === 'development') {
+  app.use(express.errorHandler());
+}
+
+routes(app);
+
+app.listen(app.get('port'), function () {
+  console.log('Express server listening on port ' + app.get('port'));
+});
